c486e314c56c529c39f4cec76ba83f41e6bce8a8
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-04-24 16:07:40 +0100

    Update to remove duplicate records commit to new_referrals.sql

--- src/extract_queries/new_referrals.sql
+++ src/extract_queries/new_referrals.sql
@@ -1,4 +1,5 @@
-SELECT REF.[AgeServReferDischDate]
+SELECT Distinct(REF.[RecordNumber])
+      ,REF.[AgeServReferDischDate]
       ,REF.[AgeServReferRecDate]
       ,REF.[ClinRespPriorityType]
       ,REF.[DecisionToTreatDate]
@@ -13,7 +14,6 @@ SELECT REF.[AgeServReferDischDate]
       ,REF.[Person_ID]
       ,REF.[PrimReasonReferralMH]
       ,REF.[RecordEndDate]
-      ,REF.[RecordNumber]
       ,REF.[RecordStartDate]
       ,REF.[ReferralRequestReceivedDate]
       ,REF.[ServDischDate]
@@ -33,20 +33,20 @@ SELECT REF.[AgeServReferDischDate]
       ,REF.[ReferRejectReason]
       ,REF.[UniqCareProfTeamLocalID]
       ,MPI.[LADistrictAuth]
-      --,SERVTD.[ServTeamTypeMH]
+      ,SERVTD.[ServTeamTypeMH]
   FROM [Reporting_MESH_MHSDS].[MHS101Referral_Published] AS REF
         
     LEFT JOIN [Reporting_MESH_MHSDS].[MHS001MPI_Published] AS MPI
 		ON REF.[RecordNumber] = MPI.[RecordNumber]
 
-    --LEFT JOIN [Reporting_MESH_MHSDS].[MHS902ServiceTeamDetails_Published] as SERVTD
-    --    ON REF.[UniqCareProfTeamLocalID] = SERVTD.[UniqCareProfTeamLocalID]
+    LEFT JOIN [Reporting_MESH_MHSDS].[MHS902ServiceTeamDetails_Published] as SERVTD
+        ON REF.[UniqCareProfTeamLocalID] = SERVTD.[UniqCareProfTeamLocalID]
 
   WHERE REF.[UniqMonthID] = (SELECT max([UniqMonthID])
                                 FROM [Reporting_MESH_MHSDS].[MHS101Referral_Published])
         AND REF.[OrgIDProv] = 'RV5'
         AND REF.[PrimReasonReferralMH] = 12
-        --AND SERVTD.[ServTeamTypeMH] = 'C10'
+        AND SERVTD.[ServTeamTypeMH] = 'C10'
         AND REF.[ReferralRequestReceivedDate] BETWEEN '01 February 2026' AND '28 February 2026'
         AND REF.[AgeServReferRecDate] >= 12
         AND MPI.[LADistrictAuth] LIKE ('E%')
