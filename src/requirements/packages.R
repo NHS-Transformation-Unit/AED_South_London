df81c8274e1cc93713e590eefe05abce76a78af8
ASW-Analyst <andy.wilson8@nhs.net>
2026-05-07 12:10:46 +0100

    Create DQ packages

---
+++ src/requirements/packages.R
@@ -0,0 +1,28 @@
+
+
+# DQ Packages -------------------------------------------------------------
+
+dq_packages <- c("readxl",
+                   "here",
+                   "odbc",
+                   "DBI",
+                   "readr",
+                   "dplyr")
+
+# load_packages function --------------------------------------------------
+
+load_packages <- function(packages){
+  
+  missing_packages <- setdiff(packages, installed.packages()[,"Package"])
+  
+  if (length(missing_packages) > 0) {
+    install.packages(missing_packages)
+  }
+  
+  lapply(packages, function(pkg) {
+    library(pkg, character.only = TRUE)
+  })
+  
+}
+
+load_packages(dq_packages)
