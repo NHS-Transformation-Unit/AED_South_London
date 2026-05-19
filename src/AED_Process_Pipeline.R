6360b186f13fdbae5b8e1ff02cb81d5e518cc123
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-19 13:40:44 +0100

    Creation of the single pipeline script commit to AED_Process_Pipeline.R

---
+++ src/AED_Process_Pipeline.R
@@ -0,0 +1,25 @@
+# Script for running all processing scripts for the AED population comparison pipeline
+
+library(here)
+
+## Loading necessary packages for processing scripts
+
+source(paste0(here(),"/src/requirements/packages.R"))
+
+## Connects to UDAL
+
+source(paste0(here(),"/src/config/connection.R"))
+
+## Load in the referral and population data
+
+source(paste0(here(),"/src/load/load_referrals.R"))
+source(paste0(here(),"/src/load/load_population.R"))
+
+## Process the loaded data
+
+source(paste0(here(),"/src/processing/ref_new_processing.R"))
+source(paste0(here(),"/src/processing/population_processing.R"))
+
+## merge processed data into a single dataframe
+
+source(paste0(here(),"/src/processing/merge_data.R"))
