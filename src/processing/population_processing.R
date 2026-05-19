d59e6f60527b5b3d9c7ac2484a4905a3404fc4ad
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-19 09:09:03 +0100

    Addition of age band and total summaries commit to population_processing.R

--- src/processing/population_processing.R
+++ src/processing/population_processing.R
@@ -1,16 +1,43 @@
 
+# Add age bands to referrals
+
+pop_proc <- population_data |>
+  mutate('Age_band' = case_when(`Age (86 categories) Code` < 12 ~ 'Under 12',
+                                between(`Age (86 categories) Code`,12,18) ~ '12 to 18',
+                                between(`Age (86 categories) Code`,19,25) ~ '19 to 25',
+                                between(`Age (86 categories) Code`,26,35) ~ '26 to 35',
+                                between(`Age (86 categories) Code`,36,45) ~ '36 to 45',
+                                between(`Age (86 categories) Code`,46,55) ~ '46 to 55',
+                                between(`Age (86 categories) Code`,56,65) ~ '56 to 65',
+                                between(`Age (86 categories) Code`,66,75) ~ '66 to 75',
+                                between(`Age (86 categories) Code`,76,85) ~ '76 to 85',
+                                `Age (86 categories) Code` > 85 ~ 'Over 85',
+                                TRUE ~ 'NA'))
+
+
 # Age banding of population
 
-pop_age <- population_data |>
+pop_age_LA <- pop_proc |>
   group_by(`Upper tier local authorities`,
-           `Age (86 categories)`) |>
-  summarise('population' = sum(`Observation`, na.rm = TRUE))
+           Age_band) |>
+  summarise('Population' = sum(`Observation`, na.rm = TRUE))
+
+
+pop_age_tot <- pop_proc |>
+  mutate("Total" = "Total") |>
+  group_by(Total,
+           Age_band) |>
+  summarise('Population' = sum(`Observation`, na.rm = TRUE)) |>
+  rename("Upper tier local authorities" = "Total")
+
+
+pop_age <- rbind(pop_age_LA,pop_age_tot)
 
 
 # Ethnicity banding of population
 
-pop_eth <- population_data |>
+pop_eth <- pop_proc |>
   group_by(`Upper tier local authorities`,
            `Ethnic group (8 categories)`) |>
-  summarise('population' = sum(`Observation`, na.rm = TRUE))
+  summarise('Population' = sum(`Observation`, na.rm = TRUE))
 
