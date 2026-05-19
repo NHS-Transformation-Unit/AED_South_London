b8c32e7623512e8f7ea21a7e8714821438f9b9d7
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-19 09:09:50 +0100

    Creation of referrals summaries commit to ref_new_processing.R

---
+++ src/processing/ref_new_processing.R
@@ -0,0 +1,41 @@
+
+# Add age bands to referrals
+
+ref_new_proc <- ref_new |>
+  mutate('Age_band' = case_when(AgeServReferRecDate < 12 ~ 'Under 12',
+                                between(AgeServReferRecDate,12,18) ~ '12 to 18',
+                                between(AgeServReferRecDate,19,25) ~ '19 to 25',
+                                between(AgeServReferRecDate,26,35) ~ '26 to 35',
+                                between(AgeServReferRecDate,36,45) ~ '36 to 45',
+                                between(AgeServReferRecDate,46,55) ~ '46 to 55',
+                                between(AgeServReferRecDate,56,65) ~ '56 to 65',
+                                between(AgeServReferRecDate,66,75) ~ '66 to 75',
+                                between(AgeServReferRecDate,76,85) ~ '76 to 85',
+                                AgeServReferRecDate > 85 ~ 'Over 85',
+                                TRUE ~ 'NA'))
+
+
+# Age banding of population
+
+ref_new_age_LA <- ref_new_proc |>
+  group_by(LAD16NM,
+           Age_band) |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE))
+
+ref_new_age_tot <- ref_new_proc |>
+  mutate("Total" = "Total") |>
+  group_by(Total,
+           Age_band) |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE)) |>
+  rename("LAD16NM" = "Total")
+
+ref_new_age <- rbind(ref_new_age_LA,ref_new_age_tot)
+
+
+# Ethnicity banding of population
+
+ref_new_eth <- ref_new_proc |>
+  group_by(LAD16NM,
+           Ethnic_Category_Main_Desc) |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE))
+
