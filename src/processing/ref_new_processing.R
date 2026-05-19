76496533c5970e519b2254e43df8383c870a2e59
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-19 13:37:35 +0100

    Addition of the calculation on standard error to the referrals data commit to ref_new_processing.R

--- src/processing/ref_new_processing.R
+++ src/processing/ref_new_processing.R
@@ -20,7 +20,12 @@ ref_new_proc <- ref_new |>
 ref_new_age_LA <- ref_new_proc |>
   group_by(LAD16NM,
            Age_band) |>
-  summarise('Referrals' = sum(New_referral, na.rm = TRUE))
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE),
+            'Mean' = mean(New_referral, na.rm = TRUE),
+            'SD' = sd(New_referral, na.rm = TRUE),
+            'Error' = SD/sqrt(Referrals),
+            'low_error' = `Mean` - `Error`,
+            'high_error' = `Mean` + `Error`)
 
 ref_new_age_tot <- ref_new_proc |>
   mutate("Total" = "Total") |>
