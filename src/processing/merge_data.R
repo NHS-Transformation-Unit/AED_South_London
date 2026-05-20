7ad9ae3b11062a1ce299f7cfcd713d21b7afc644
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-20 16:17:43 +0100

    Removal of unused code commit to merge_data.R

--- src/processing/merge_data.R
+++ src/processing/merge_data.R
@@ -1,13 +1,6 @@
 
 # Merge of population data with referrals for comparison
 
-unified_age <- left_join(pop_age, ref_new_age, by = c("Upper tier local authorities" = "LAD16NM", "Age_band" = "Age_band"))
-
-unified_age_LA_per <- left_join(pop_LA_per, ref_new_LA_per, by = c("Upper tier local authorities" = "LAD16NM", 
-                                                                "Age_band" = "Age_band")) |>
-  rename("Pop_per" = "Percentage.x",
-         "Ref_per" = "Percentage.y")
-
 unified_age_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier local authorities" = "LAD16NM", 
                                                                    "Age_band" = "Age_band")) |>
   rename("Pop_per" = "Percentage.x",
