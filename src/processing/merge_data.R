740ebda83799796851513ad9f21a52d6ddb1508e
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-20 14:39:48 +0100

    Addition of South London boroughs into filter for final data frame, and creation of a London only final data frame commit to merge_data.R

--- src/processing/merge_data.R
+++ src/processing/merge_data.R
@@ -16,4 +16,31 @@ unified_age_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier local 
          `Age_band`,
          `Pop_per`,
          `Ref_per`,
-         `Confidence`)
+         `Confidence`) |>
+  filter(`Upper tier local authorities` %in% c("Bexley",
+                                               "Bromley",
+                                               "Greenwich",
+                                               "Lambeth",
+                                               "Lewisham",
+                                               "Southwark",
+                                               "Croydon",
+                                               "Kingston upon Thames",
+                                               "Merton",
+                                               "Richmond upon Thames",
+                                               "Sutton",
+                                               "Wandsworth"),
+         Age_band != "Under 12")
+
+
+unified_age_LON_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier local authorities" = "LAD16NM", 
+                                                              "Age_band" = "Age_band")) |>
+  rename("Pop_per" = "Percentage.x",
+         "Ref_per" = "Percentage.y") |>
+  select(`Upper tier local authorities`,
+         `Age_band`,
+         `Pop_per`,
+         `Ref_per`,
+         `Confidence`) |>
+  filter(`Upper tier local authorities` == "London",
+         Age_band != "Under 12")
+
