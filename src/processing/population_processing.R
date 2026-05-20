ff5206cee6c34a21fecd8d708ad07794212876d5
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-20 12:57:27 +0100

    Addition of the percentage per age band at the London aggregate level commit to population_processing.R

--- src/processing/population_processing.R
+++ src/processing/population_processing.R
@@ -31,11 +31,20 @@ pop_LA_per <- left_join(pop_age_LA, pop_LA, by = c('Upper tier local authorities
 
 
 pop_age_tot <- pop_proc |>
-  mutate("Total" = "Total") |>
+  mutate("Total" = "London") |>
   group_by(Total,
            Age_band) |>
   summarise('Population' = sum(`Observation`, na.rm = TRUE)) |>
   rename("Upper tier local authorities" = "Total")
+
+pop_tot <- pop_proc |>
+  mutate("Total" = "London") |>
+  group_by(Total) |>
+  summarise('London_Population' = sum(`Observation`, na.rm = TRUE)) |>
+  rename("Upper tier local authorities" = "Total")
+
+pop_tot_per <- left_join(pop_age_tot, pop_tot, by = c('Upper tier local authorities' = 'Upper tier local authorities')) |>
+  mutate("Percentage" = Population/London_Population)
 
 
 pop_age <- rbind(pop_age_LA,pop_age_tot)
