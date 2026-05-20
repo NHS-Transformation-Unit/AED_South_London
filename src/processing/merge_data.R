7fbc749a1388640ad09b32a432f8c310c772593b
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-20 13:59:16 +0100

    Creating of unified age band, LAD and percentages data frame for visualisation commit to merge_data.R

--- src/processing/merge_data.R
+++ src/processing/merge_data.R
@@ -3,7 +3,17 @@
 
 unified_age <- left_join(pop_age, ref_new_age, by = c("Upper tier local authorities" = "LAD16NM", "Age_band" = "Age_band"))
 
-unified_age_per <- left_join(pop_LA_per, ref_new_LA_per, by = c("Upper tier local authorities" = "LAD16NM", 
+unified_age_LA_per <- left_join(pop_LA_per, ref_new_LA_per, by = c("Upper tier local authorities" = "LAD16NM", 
                                                                 "Age_band" = "Age_band")) |>
   rename("Pop_per" = "Percentage.x",
          "Ref_per" = "Percentage.y")
+
+unified_age_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier local authorities" = "LAD16NM", 
+                                                                   "Age_band" = "Age_band")) |>
+  rename("Pop_per" = "Percentage.x",
+         "Ref_per" = "Percentage.y") |>
+  select(`Upper tier local authorities`,
+         `Age_band`,
+         `Pop_per`,
+         `Ref_per`,
+         `Confidence`)
