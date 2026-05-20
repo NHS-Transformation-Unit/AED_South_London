bebc9f28c219dbacbcc55d02f715fbea2411c1e2
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-20 16:16:00 +0100

    Update to layout and addition of Ethnicity calculations commit to population_processing.R

--- src/processing/population_processing.R
+++ src/processing/population_processing.R
@@ -22,14 +22,6 @@ pop_age_LA <- pop_proc |>
            Age_band) |>
   summarise('Population' = sum(`Observation`, na.rm = TRUE))
 
-pop_LA <- pop_proc |>
-  group_by(`Upper tier local authorities`) |>
-  summarise('LAD_Population' = sum(`Observation`, na.rm = TRUE))
-
-pop_LA_per <- left_join(pop_age_LA, pop_LA, by = c('Upper tier local authorities' = 'Upper tier local authorities')) |>
-  mutate("Percentage" = Population/LAD_Population)
-
-
 pop_age_tot <- pop_proc |>
   mutate("Total" = "London") |>
   group_by(Total,
@@ -37,11 +29,41 @@ pop_age_tot <- pop_proc |>
   summarise('Population' = sum(`Observation`, na.rm = TRUE)) |>
   rename("Upper tier local authorities" = "Total")
 
+
+# Ethnicity banding of population
+
+pop_eth_LA <- pop_proc |>
+  group_by(`Upper tier local authorities`,
+           `Ethnic group (8 categories)`) |>
+  summarise('Population' = sum(`Observation`, na.rm = TRUE))
+
+pop_eth_tot <- pop_proc |>
+  mutate("Total" = "London") |>
+  group_by(Total,
+           `Ethnic group (8 categories)`) |>
+  summarise('Population' = sum(`Observation`, na.rm = TRUE)) |>
+  rename("Upper tier local authorities" = "Total")
+
+
+
+# Totals ------------------------------------------------------------------
+
+pop_LA <- pop_proc |>
+  group_by(`Upper tier local authorities`) |>
+  summarise('LAD_Population' = sum(`Observation`, na.rm = TRUE))
+
 pop_tot <- pop_proc |>
   mutate("Total" = "London") |>
   group_by(Total) |>
   summarise('LAD_Population' = sum(`Observation`, na.rm = TRUE)) |>
   rename("Upper tier local authorities" = "Total")
+
+
+
+# Age - Population percentages --------------------------------------------------
+
+pop_LA_per <- left_join(pop_age_LA, pop_LA, by = c('Upper tier local authorities' = 'Upper tier local authorities')) |>
+  mutate("Percentage" = Population/LAD_Population)
 
 pop_tot_per <- left_join(pop_age_tot, pop_tot, by = c('Upper tier local authorities' = 'Upper tier local authorities')) |>
   mutate("Percentage" = Population/LAD_Population)
@@ -50,10 +72,14 @@ pop_tot_per <- left_join(pop_age_tot, pop_tot, by = c('Upper tier local authorit
 pop_age_per <- rbind(pop_LA_per,pop_tot_per)
 
 
-# Ethnicity banding of population
 
-pop_eth <- pop_proc |>
-  group_by(`Upper tier local authorities`,
-           `Ethnic group (8 categories)`) |>
-  summarise('Population' = sum(`Observation`, na.rm = TRUE))
+# Ethnicity - Population percentages --------------------------------------
 
+pop_eth_LA_per <- left_join(pop_eth_LA, pop_LA, by = c('Upper tier local authorities' = 'Upper tier local authorities')) |>
+  mutate("Percentage" = Population/LAD_Population)
+
+pop_eth_tot_per <- left_join(pop_eth_tot, pop_tot, by = c('Upper tier local authorities' = 'Upper tier local authorities')) |>
+  mutate("Percentage" = Population/LAD_Population)
+
+
+pop_eth_per <- rbind(pop_eth_LA_per,pop_eth_tot_per)
