8fefd94b2bd5ff141ef554209752d3f53ad6e93f
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-05-20 12:41:36 +0100

    Update to age_band calculation and addition of percentage to London population table commit to population_processing.R

--- src/processing/population_processing.R
+++ src/processing/population_processing.R
@@ -3,14 +3,14 @@
 
 pop_proc <- population_data |>
   mutate('Age_band' = case_when(`Age (86 categories) Code` < 12 ~ 'Under 12',
-                                between(`Age (86 categories) Code`,12,18) ~ '12 to 18',
-                                between(`Age (86 categories) Code`,19,25) ~ '19 to 25',
-                                between(`Age (86 categories) Code`,26,35) ~ '26 to 35',
-                                between(`Age (86 categories) Code`,36,45) ~ '36 to 45',
-                                between(`Age (86 categories) Code`,46,55) ~ '46 to 55',
-                                between(`Age (86 categories) Code`,56,65) ~ '56 to 65',
-                                between(`Age (86 categories) Code`,66,75) ~ '66 to 75',
-                                between(`Age (86 categories) Code`,76,85) ~ '76 to 85',
+                                `Age (86 categories) Code` < 19 ~ '12 to 18',
+                                `Age (86 categories) Code` < 26 ~ '19 to 25',
+                                `Age (86 categories) Code` < 36 ~ '26 to 35',
+                                `Age (86 categories) Code` < 46 ~ '36 to 45',
+                                `Age (86 categories) Code` < 56 ~ '46 to 55',
+                                `Age (86 categories) Code` < 66 ~ '56 to 65',
+                                `Age (86 categories) Code` < 76 ~ '66 to 75',
+                                `Age (86 categories) Code` < 86 ~ '76 to 85',
                                 `Age (86 categories) Code` > 85 ~ 'Over 85',
                                 TRUE ~ 'NA'))
 
@@ -21,6 +21,13 @@ pop_age_LA <- pop_proc |>
   group_by(`Upper tier local authorities`,
            Age_band) |>
   summarise('Population' = sum(`Observation`, na.rm = TRUE))
+
+pop_LA <- pop_proc |>
+  group_by(`Upper tier local authorities`) |>
+  summarise('LAD_Population' = sum(`Observation`, na.rm = TRUE))
+
+pop_LA_per <- left_join(pop_age_LA, pop_LA, by = c('Upper tier local authorities' = 'Upper tier local authorities')) |>
+  mutate("Percentage" = Population/LAD_Population)
 
 
 pop_age_tot <- pop_proc |>
