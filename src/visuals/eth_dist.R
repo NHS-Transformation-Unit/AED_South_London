f2d1d01b9de3e67a85a7606dee22c00e788f18de
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-01 10:46:30 +0100

    Creation of ethnicity grouping distribution visual.

---
+++ src/visuals/eth_dist.R
@@ -0,0 +1,64 @@
+
+# Ethnic group by LAD distribution
+
+plot_eth <- unified_eth_per |>
+  pivot_longer(cols = c(Pop_per, Ref_per),
+               names_to = "Series",
+               values_to = "Percent")
+
+ggplot(plot_eth, aes(x = `Ethnic group (8 categories)`,
+                     y = Percent,
+                     fill = Series)) +
+  geom_col(position = position_dodge(width = 0.8),
+           width = 0.7) +
+  geom_errorbar(data = subset(plot_eth, Series == "Ref_per"),
+                aes(ymin = Percent - Confidence,
+                    ymax = Percent + Confidence),
+                position = position_nudge(x = 0.2),
+                width = 0.2) +
+  facet_wrap(~ `Upper tier local authorities`) +
+  scale_fill_manual(values = c("Ref_per" = "blue",
+                               "Pop_per" = "red"),
+                    labels = c("Borough population distribution",
+                               "New referrals")) +
+  labs(x = "Ethnic group",
+       y = "Percentage",
+       fill = "",
+       title = "South London Boroughs") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  theme(axis.text.x = element_text(angle = 90,
+                                   vjust = 0.5,
+                                   hjust = 1))
+
+
+
+# Age band for London distribution
+
+plot_LON_eth <- unified_eth_LON_per |>
+  pivot_longer(cols = c(Pop_per, Ref_per),
+               names_to = "Series",
+               values_to = "Percent")
+
+ggplot(plot_LON_eth, aes(x = `Ethnic group (8 categories)`,
+                         y = Percent,
+                         fill = Series)) +
+  geom_col(position = position_dodge(width = 0.8),
+           width = 0.7) +
+  geom_errorbar(data = subset(plot_LON_eth, Series == "Ref_per"),
+                aes(ymin = Percent - Confidence,
+                    ymax = Percent + Confidence),
+                position = position_nudge(x = 0.2),
+                width = 0.2) +
+  scale_fill_manual(values = c("Ref_per" = "blue",
+                               "Pop_per" = "red"),
+                    labels = c("South London population distribution",
+                               "New referrals")) +
+  labs(x = "Ethnic group",
+       y = "Percentage",
+       fill = "",
+       title = "South London total") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  theme(axis.text.x = element_text(angle = 90,
+                                   vjust = 0.5,
+                                   hjust = 1))
+
