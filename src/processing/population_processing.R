746a7ed60870cedf215ba4a4a5f4efbd00612730
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-01 08:53:47 +0100

    Update to age bands and addition of filtering for relevant local authorities commit to population_processing.R

--- src/processing/population_processing.R
+++ src/processing/population_processing.R
@@ -3,8 +3,8 @@
 
 pop_proc <- population_data |>
   mutate('Age_band' = case_when(`Age (86 categories) Code` < 12 ~ 'Under 12',
-                                `Age (86 categories) Code` < 19 ~ '12 to 18',
-                                `Age (86 categories) Code` < 26 ~ '19 to 25',
+                                `Age (86 categories) Code` < 18 ~ '12 to 17',
+                                `Age (86 categories) Code` < 26 ~ '18 to 25',
                                 `Age (86 categories) Code` < 36 ~ '26 to 35',
                                 `Age (86 categories) Code` < 46 ~ '36 to 45',
                                 `Age (86 categories) Code` < 56 ~ '46 to 55',
@@ -12,7 +12,19 @@ pop_proc <- population_data |>
                                 `Age (86 categories) Code` < 76 ~ '66 to 75',
                                 `Age (86 categories) Code` < 86 ~ '76 to 85',
                                 `Age (86 categories) Code` > 85 ~ 'Over 85',
-                                TRUE ~ 'NA'))
+                                TRUE ~ 'NA')) |>
+  filter(`Upper tier local authorities` %in% c("Bexley",
+                                               "Bromley",
+                                               "Greenwich",
+                                               "Lambeth",
+                                               "Lewisham",
+                                               "Southwark",
+                                               "Croydon",
+                                               "Kingston upon Thames",
+                                               "Merton",
+                                               "Richmond upon Thames",
+                                               "Sutton",
+                                               "Wandsworth"))
 
 
 # Age banding of population
