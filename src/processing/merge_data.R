f20e5653c01071bfed0c7420d811a92cc1adb9fc
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-01 10:45:59 +0100

    Removal of LA filter from merge stage and addition of ethnic group data merges commit to merge_data.R

--- src/processing/merge_data.R
+++ src/processing/merge_data.R
@@ -1,5 +1,5 @@
 
-# Merge of population data with referrals for comparison
+# Merge of age population data with referrals for comparison
 
 unified_age_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier local authorities" = "LAD16NM", 
                                                                    "Age_band" = "Age_band")) |>
@@ -10,19 +10,7 @@ unified_age_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier local 
          `Pop_per`,
          `Ref_per`,
          `Confidence`) |>
-  filter(`Upper tier local authorities` %in% c("Bexley",
-                                               "Bromley",
-                                               "Greenwich",
-                                               "Lambeth",
-                                               "Lewisham",
-                                               "Southwark",
-                                               "Croydon",
-                                               "Kingston upon Thames",
-                                               "Merton",
-                                               "Richmond upon Thames",
-                                               "Sutton",
-                                               "Wandsworth"),
-         Age_band != "Under 12")
+  filter(Age_band != "Under 12")
 
 
 unified_age_LON_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier local authorities" = "LAD16NM", 
@@ -36,4 +24,29 @@ unified_age_LON_per <- left_join(pop_age_per, ref_new_per, by = c("Upper tier lo
          `Confidence`) |>
   filter(`Upper tier local authorities` == "London",
          Age_band != "Under 12")
+
+
+# Merge of ethnicity population data with referrals for comparison
+
+unified_eth_per <- left_join(pop_eth_per, ref_new_eth_per, by = c("Upper tier local authorities" = "LAD16NM",
+                                                                  "Ethnic group (8 categories)" = "Ethnic_group")) |>
+  rename("Pop_per" = "Percentage.x",
+         "Ref_per" = "Percentage.y") |>
+  select(`Upper tier local authorities`,
+         `Ethnic group (8 categories)`,
+         `Pop_per`,
+         `Ref_per`,
+         `Confidence`)
+
+
+unified_eth_LON_per <- left_join(pop_eth_per, ref_new_eth_per, by = c("Upper tier local authorities" = "LAD16NM",
+                                                                      "Ethnic group (8 categories)" = "Ethnic_group")) |>
+  rename("Pop_per" = "Percentage.x",
+         "Ref_per" = "Percentage.y") |>
+  select(`Upper tier local authorities`,
+         `Ethnic group (8 categories)`,
+         `Pop_per`,
+         `Ref_per`,
+         `Confidence`) |>
+  filter(`Upper tier local authorities` == "London")
 
