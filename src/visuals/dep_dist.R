6e1aa65c33aaae0107332f6ad78a4687d7450d5a
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-03 12:29:30 +0100

    Creation of deprivation visuals.

---
+++ src/visuals/dep_dist.R
@@ -0,0 +1,61 @@
+
+# Deprivation band by LAD distribution
+
+plot_dep <- unified_dep_LA_per |>
+  pivot_longer(cols = c(Pop_per, Ref_per),
+               names_to = "Series",
+               values_to = "Percent")
+
+ggplot(plot_dep, aes(x = `IMD Decile`,
+                     y = Percent,
+                     fill = Series)) +
+  geom_col(position = position_dodge(width = 0.8),
+           width = 0.7) +
+  geom_errorbar(data = subset(plot_dep, Series == "Ref_per"),
+                aes(ymin = Percent - Confidence,
+                    ymax = Percent + Confidence),
+                position = position_nudge(x = 0.2),
+                width = 0.2) +
+  facet_wrap(~ `Upper tier local authorities`) +
+  scale_fill_manual(name = "Population Group",
+                    values = c("Ref_per" = palette_tu[4],
+                               "Pop_per" = palette_tu[5]),
+                    labels = c("Borough population distribution",
+                               "New referrals")) +
+  labs(x = "IMD Decile",
+       y = "Percentage",
+       title = "South London Boroughs") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  scale_x_continuous(limits = c(1,10), breaks = 1:10) +
+  theme(selected_theme(palette_tu[1]))
+
+
+# Deprivation band for London distribution
+
+plot_LON_dep <- unified_dep_LON_per |>
+  pivot_longer(cols = c(Pop_per, Ref_per),
+               names_to = "Series",
+               values_to = "Percent")
+
+ggplot(plot_LON_dep, aes(x = `IMD Decile`,
+                         y = Percent,
+                         fill = Series)) +
+  geom_col(position = position_dodge(width = 0.8),
+           width = 0.7) +
+  geom_errorbar(data = subset(plot_LON_dep, Series == "Ref_per"),
+                aes(ymin = Percent - Confidence,
+                    ymax = Percent + Confidence),
+                position = position_nudge(x = 0.2),
+                width = 0.2) +
+  scale_fill_manual(name = "Population Group",
+                    values = c("Ref_per" = palette_tu[4],
+                               "Pop_per" = palette_tu[5]),
+                    labels = c("South London population distribution",
+                               "New referrals")) +
+  labs(x = "IMD Decile",
+       y = "Percentage",
+       title = "South London total") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  scale_x_continuous(limits = c(1,10), breaks = 1:10) +
+  theme(selected_theme(palette_tu[1]))
+
