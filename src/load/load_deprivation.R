2f42fcffe4c9839b9ec460769093f62a74d667ce
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-03 10:42:05 +0100

    Load of deprivation dataframe

---
+++ src/load/load_deprivation.R
@@ -0,0 +1,16 @@
+
+deprivation_data <- read_csv(here("src",
+                                 "reference_data",
+                                 "Deprivation.csv")) |>
+  filter(`Local Authority District code (2024)` %in% c('E09000004', # Bexley
+                          'E09000006', # Bromley
+                          'E09000008', # Croydon
+                          'E09000011', # Greenwich
+                          'E09000021', # Kingston upon Thames
+                          'E09000022', # Lambeth
+                          'E09000023', # Lewisham
+                          'E09000024', # Merton
+                          'E09000027', # Richmond
+                          'E09000028', # Southwark
+                          'E09000029', # Sutton
+                          'E09000032')) # Wandsworth
