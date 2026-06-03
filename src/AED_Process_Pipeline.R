41430ca13484aaffc2ab60d35be1c9579d1219ad
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-03 14:11:18 +0100

    Addition of the diagnosis visualisation commit to AED_Process_Pipeline.R

--- src/AED_Process_Pipeline.R
+++ src/AED_Process_Pipeline.R
@@ -31,6 +31,7 @@ source(paste0(here(),"/src/processing/merge_data.R"))
 ## Generate visuals
 
 source(paste0(here(),"/src/visuals/age_dist.R"))
+source(paste0(here(),"/src/visuals/diag_dist.R"))
 source(paste0(here(),"/src/visuals/dep_dist.R"))
 source(paste0(here(),"/src/visuals/eth_dist.R"))
 
