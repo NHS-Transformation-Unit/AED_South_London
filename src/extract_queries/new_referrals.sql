6a64769002df43f915cc60254d079410b87277cc
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-03 14:06:47 +0100

    Addition of join to diagnosis description commit to new_referrals.sql

--- src/extract_queries/new_referrals.sql
+++ src/extract_queries/new_referrals.sql
@@ -1,4 +1,11 @@
 
-SELECT * 
-FROM #temp_new_refs_diags
+SELECT tnrd.*,
+    DIAGDESC.Description
+    
+FROM #temp_new_refs_diags AS tnrd
+
+LEFT JOIN [UKHD_ICD10].[Codes_And_Titles_And_MetaData] AS DIAGDESC
+        ON tnrd.[PrimaryDiag] = DIAGDESC.[Alt_Code]
+        AND DIAGDESC.[ICD_Version] = 'ICD10 5th Edition'
+        
 WHERE [EarliestDiag] = 1
