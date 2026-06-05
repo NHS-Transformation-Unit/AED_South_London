a78c39e72f1c654b3153d19b78d5bdc9e27bbe6b
SiWickham <simon.wickham1@nhs.net>
2026-06-05 15:12:07 +0100

    Addition of visual themes and data table, and naming them as objects commit to eth_dist.R

--- src/visuals/eth_dist.R
+++ src/visuals/eth_dist.R
@@ -1,4 +1,6 @@
 
+# Borough -----------------------------------------------------------------
+
 # Ethnic group by LAD distribution
 
 plot_eth <- unified_eth_LA_per |>
@@ -29,10 +31,39 @@ LA_eth <- ggplot(plot_eth, aes(x = `Ethnic group (8 categories)`,
        caption = "Source: Mental Health Services Data Set and Office of National Statistics") +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   scale_x_discrete(labels = function(Diagnosis) str_wrap(Diagnosis, width = 20)) +
-  theme(selected_theme(palette_tu[1]),
-        legend.position = "bottom")
+  theme(text = element_text(family = "Franklin Gothic Book"),
+        strip.background = element_rect(fill = palette_tu[1]),
+        strip.text = element_text(colour = "black", size = 10),
+        axis.text = element_text(size = 10),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.title = element_text(size = 11),
+        plot.title = element_text(size = 16, color = palette_tu[1]),
+        plot.subtitle = element_text(size = 12),
+        panel.background = element_rect(fill = "#ffffff"),
+        panel.grid.major.y = element_line(color = "#cecece", linewidth = 0.1),
+        panel.grid.minor.y = element_blank(),
+        axis.line = element_line(color = "#000000"),
+        legend.position = "bottom",
+        legend.text = element_text(size = 7.5))
 
 
+# Ethnic group by LA table
+
+LA_eth_tbl <- unified_eth_LA_per |>
+  gt() |>
+  fmt_percent(columns = c(Pop_per, Ref_per),
+              decimals = 1) |>
+  cols_hide(columns = Confidence) |>
+  cols_label(`Upper tier local authorities` = "Borough",
+             `Ethnic group (8 categories)` = "Ethnic group",
+             Pop_per = "Population",
+             Ref_per = "Referrals") |>
+  tab_header(title = "Distribution of South London Boroughs referrals compared to the area population") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
+
+
+# South London ------------------------------------------------------------
 
 # Age band for London distribution
 
@@ -62,8 +93,33 @@ Lon_eth <- ggplot(plot_LON_eth, aes(x = `Ethnic group (8 categories)`,
        subtitle = "Referrals received between May 2023 and April 2026",
        caption = "Source: Mental Health Services Data Set and Office of National Statistics") +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
-  scale_x_discrete(labels = function(Diagnosis) str_wrap(Diagnosis, width = 20)) +
-  theme(selected_theme(palette_tu[1]),
-        legend.position = "bottom") +
+  scale_x_discrete(labels = function(Diagnosis) str_wrap(Diagnosis, width = 35)) +
+  theme(text = element_text(family = "Franklin Gothic Book"),
+        axis.text = element_text(size = 10),
+        axis.text.x = element_text(angle = 0, vjust = 0.5, hjust = 1),
+        axis.title = element_text(size = 11),
+        plot.title = element_text(size = 16, color = palette_tu[1]),
+        plot.subtitle = element_text(size = 12),
+        panel.background = element_rect(fill = "#ffffff"),
+        panel.grid.major.y = element_line(color = "#cecece", linewidth = 0.1),
+        panel.grid.minor.y = element_blank(),
+        axis.line = element_line(color = "#000000"),
+        legend.position = "bottom",
+        legend.text = element_text(size = 7.5)) +
   coord_flip()
 
+
+# Ethnic group for London table
+
+Lon_eth_tbl <- unified_eth_LON_per |>
+  gt() |>
+  fmt_percent(columns = c(Pop_per, Ref_per),
+              decimals = 1) |>
+  cols_hide(columns = c(Confidence,
+                        `Upper tier local authorities`)) |>
+  cols_label(`Ethnic group (8 categories)` = "Ethnic group",
+             Pop_per = "Population",
+             Ref_per = "Referrals") |>
+  tab_header(title = "Distribution of South London total referrals compared to the area population") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
