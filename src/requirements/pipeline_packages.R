df836d45dd7cb7101ace18735762816306802c2c
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-05 10:04:06 +0100

    creation of packages to run the pipeline.

---
+++ src/requirements/pipeline_packages.R
@@ -0,0 +1,33 @@
+
+
+# DQ Packages -------------------------------------------------------------
+
+pipeline_packages <- c("readxl",
+              "here",
+              "odbc",
+              "DBI",
+              "readr",
+              "dplyr",
+              "tidyr",
+              "stringr",
+              "knitr",
+              "ggplot2",
+              "gt")
+
+# load_packages function --------------------------------------------------
+
+load_packages <- function(packages){
+  
+  missing_packages <- setdiff(packages, installed.packages()[,"Package"])
+  
+  if (length(missing_packages) > 0) {
+    install.packages(missing_packages)
+  }
+  
+  lapply(packages, function(pkg) {
+    library(pkg, character.only = TRUE)
+  })
+  
+}
+
+load_packages(pipeline_packages)
