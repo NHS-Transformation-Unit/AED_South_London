6f1474656aa0f65c859a6c05cc871a4eeb26a289
SiWickham <simon.wickham1@nhs.net>
2026-06-05 16:00:54 +0100

    Change to titles font size and facet wrap columns commit to dep_dist.R

--- src/visuals/dep_dist.R
+++ src/visuals/dep_dist.R
@@ -18,7 +18,8 @@ LA_dep <- ggplot(plot_dep, aes(x = `IMD Decile`,
                     ymax = Percent + Confidence),
                 position = position_nudge(x = 0.2),
                 width = 0.2) +
-  facet_wrap(~ `Upper tier local authorities`) +
+  facet_wrap(~ `Upper tier local authorities`,
+             ncol = 3) +
   scale_fill_manual(name = "Population Group",
                     values = c("Ref_per" = palette_tu[4],
                                "Pop_per" = palette_tu[5]),
@@ -36,20 +37,22 @@ LA_dep <- ggplot(plot_dep, aes(x = `IMD Decile`,
         strip.text = element_text(colour = "black", size = 10),
         axis.text = element_text(size = 10),
         axis.text.x = element_text(angle = 0, vjust = 0.5, hjust = 1),
-        axis.title = element_text(size = 11),
-        plot.title = element_text(size = 16, color = palette_tu[1]),
-        plot.subtitle = element_text(size = 12),
+        axis.title = element_text(size = 10),
+        plot.title = element_text(size = 12, color = palette_tu[1]),
+        plot.subtitle = element_text(size = 10),
         panel.background = element_rect(fill = "#ffffff"),
         panel.grid.major.y = element_line(color = "#cecece", linewidth = 0.1),
         panel.grid.minor.y = element_blank(),
         axis.line = element_line(color = "#000000"),
         legend.position = "bottom",
-        legend.text = element_text(size = 7.5))
+        legend.text = element_text(size = 7.5)) +
+  coord_flip()
 
 
 # Deprivation band by LA table
 
 LA_dep_tbl <- unified_dep_LA_per |>
+  ungroup() |>
   gt() |>
   fmt_percent(columns = c(Pop_per, Ref_per),
               decimals = 1) |>
@@ -97,9 +100,9 @@ Lon_dep <- ggplot(plot_LON_dep, aes(x = `IMD Decile`,
   theme(text = element_text(family = "Franklin Gothic Book"),
         axis.text = element_text(size = 10),
         axis.text.x = element_text(angle = 0, vjust = 0.5, hjust = 1),
-        axis.title = element_text(size = 11),
-        plot.title = element_text(size = 16, color = palette_tu[1]),
-        plot.subtitle = element_text(size = 12),
+        axis.title = element_text(size = 10),
+        plot.title = element_text(size = 12, color = palette_tu[1]),
+        plot.subtitle = element_text(size = 10),
         panel.background = element_rect(fill = "#ffffff"),
         panel.grid.major.y = element_line(color = "#cecece", linewidth = 0.1),
         panel.grid.minor.y = element_blank(),
@@ -112,6 +115,7 @@ Lon_dep <- ggplot(plot_LON_dep, aes(x = `IMD Decile`,
 # Deprivation band for London table
 
 Lon_dep_tbl <- unified_dep_LON_per |>
+  ungroup() |>
   gt() |>
   fmt_percent(columns = c(Pop_per,
                           Ref_per),
