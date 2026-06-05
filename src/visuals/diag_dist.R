62e9372a434a12a893eabafdc9f619bbc84575b5
Simon Wickham <simon.wickham1@udal.nhs.uk>
2026-06-05 11:20:26 +0100

    Assign visuals to an object for calling in qmd commit to diag_dist.R

--- src/visuals/diag_dist.R
+++ src/visuals/diag_dist.R
@@ -1,14 +1,15 @@
 
 # Diagnosis distribution
 
-ggplot(ref_new_diag, aes(x = reorder(Diagnosis,Diagnosed),
-                         y = Diagnosed,
-                         fill = colour)) +
+Ref_diag <- ggplot(ref_new_diag, aes(x = reorder(Diagnosis,Diagnosed),
+                                     y = Diagnosed,
+                                     fill = colour)) +
   geom_col() +
   scale_fill_identity() +
   scale_x_discrete(labels = function(Diagnosis) str_wrap(Diagnosis, width = 20)) +
   labs(x = "Diagnosis description",
        y = "Number of diagnoses",
-       title = "South London") +
-  theme(selected_theme(palette_tu[1]),
-        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+       title = "Diagnosis received for all patients referred",
+       subtitle = "Referrals received between May 2023 and April 2026",
+       caption = "Source: Mental Health Services Data Set") +
+  theme(selected_theme(palette_tu[1]))
