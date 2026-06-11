dc10a918ec6908406745a829a429372fa257be4d
SiWickham <simon.wickham1@nhs.net>
2026-06-11 13:31:45 +0100

    Converstion of LA plot to function for reproduction in qmd commit to dep_dist.R

--- src/visuals/dep_dist.R
+++ src/visuals/dep_dist.R
@@ -1,16 +1,19 @@
 
 # Borough -----------------------------------------------------------------
 
+plot_LA_dep <- function(df, borough) {
+
 # Deprivation band by LAD distribution
 
-plot_dep <- unified_dep_LA_per |>
+plot_dep <- df |>
+  filter(`Upper tier local authorities` == borough) |>
   pivot_longer(cols = c(Pop_per, Ref_per),
                names_to = "Series",
                values_to = "Percent")
 
-LA_dep <- ggplot(plot_dep, aes(x = `IMD Decile`,
-                               y = Percent,
-                               fill = Series)) +
+ggplot(plot_dep, aes(x = `IMD Decile`,
+                     y = Percent,
+                     fill = Series)) +
   geom_col(position = position_dodge(width = 0.8),
            width = 0.7) +
   geom_errorbar(data = subset(plot_dep, Series == "Ref_per"),
@@ -18,8 +21,6 @@ LA_dep <- ggplot(plot_dep, aes(x = `IMD Decile`,
                     ymax = Percent + Confidence),
                 position = position_nudge(x = 0.2),
                 width = 0.2) +
-  facet_wrap(~ `Upper tier local authorities`,
-             ncol = 4) +
   scale_fill_manual(name = "Population Group",
                     values = c("Ref_per" = palette_tu[4],
                                "Pop_per" = palette_tu[5]),
@@ -27,12 +28,11 @@ LA_dep <- ggplot(plot_dep, aes(x = `IMD Decile`,
                                "New referrals")) +
   labs(x = "IMD Decile",
        y = "Percentage",
-       title = "Distribution of South London Boroughs referrals compared to the area population",
+       title = paste0("Distribution of ", borough," referrals compared to the area population"),
        subtitle = "Referrals received between May 2023 and April 2026",
        caption = "Source: Mental Health Services Data Set and Office of National Statistics") +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1),
-                     breaks = seq(0, 0.4, by = 0.2)) +
-  scale_x_continuous(breaks = seq(1, 10, by = 2)) +
+                     breaks = seq(0, 0.8, by = 0.1)) +
   theme(text = element_text(family = "Franklin Gothic Book"),
         strip.background = element_rect(fill = palette_tu[1]),
         strip.text = element_text(colour = "black", size = 10),
@@ -48,23 +48,31 @@ LA_dep <- ggplot(plot_dep, aes(x = `IMD Decile`,
         legend.position = "bottom",
         legend.text = element_text(size = 7.5)) +
   coord_flip()
-
+}
 
 # Deprivation band by LA table
 
 LA_dep_tbl <- unified_dep_LA_per |>
   ungroup() |>
-  gt() |>
-  fmt_percent(columns = c(Pop_per, Ref_per),
-              decimals = 1) |>
-  cols_hide(columns = Confidence) |>
-  cols_label(`Upper tier local authorities` = "Borough",
-             `IMD Decile` = "IMD Decile",
-             Pop_per = "Population",
-             Ref_per = "Referrals") |>
-  tab_header(title = "Distribution of South London Boroughs referrals compared to the area population") |>
-  tab_style(style = list(cell_fill(color = palette_tu[1])),
-            locations = cells_column_labels(everything()))
+  select(`Upper tier local authorities`,
+         `IMD Decile`,
+         Pop_per,
+         Ref_per) |>
+  rename(Borough = `Upper tier local authorities`,
+         Population = Pop_per,
+         Referrals = Ref_per) |>
+  datatable(caption = "Distribution of South London Boroughs referrals compared to the area population",
+            rownames = FALSE,
+            options = list(initComplete = JS(sprintf("function(settings, json) {
+                                                                                $(this.api().table().header()).find('th').css({
+                                                                                'background-color': '%s',
+                                                                                'color': 'white'
+                                                                                });
+                                                     }",
+                                                     palette_tu[1])))) |>
+  formatPercentage(columns = c("Population",
+                               "Referrals"),
+                   digits = 1)
 
 
 # South London -------------------------------------------------------------
@@ -117,15 +125,20 @@ Lon_dep <- ggplot(plot_LON_dep, aes(x = `IMD Decile`,
 
 Lon_dep_tbl <- unified_dep_LON_per |>
   ungroup() |>
-  gt() |>
-  fmt_percent(columns = c(Pop_per,
-                          Ref_per),
-              decimals = 1) |>
-  cols_hide(columns = c(Confidence,
-                        `Upper tier local authorities`)) |>
-  cols_label(`IMD Decile` = "IMD Decile",
-             Pop_per = "Population",
-             Ref_per = "Referrals") |>
-  tab_header(title = "Distribution of South London total referrals compared to the area population") |>
-  tab_style(style = list(cell_fill(color = palette_tu[1])),
-            locations = cells_column_labels(everything()))
+  select(`IMD Decile`,
+         Pop_per,
+         Ref_per) |>
+  rename(Population = Pop_per,
+         Referrals = Ref_per) |>
+  datatable(caption = "Distribution of South London referrals compared to the area population",
+            rownames = FALSE,
+            options = list(initComplete = JS(sprintf("function(settings, json) {
+                                                                                $(this.api().table().header()).find('th').css({
+                                                                                'background-color': '%s',
+                                                                                'color': 'white'
+                                                                                });
+                                                     }",
+                                                     palette_tu[1])))) |>
+  formatPercentage(columns = c("Population",
+                               "Referrals"),
+                   digits = 1)
