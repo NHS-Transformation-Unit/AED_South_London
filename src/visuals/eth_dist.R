3da3fb36ad81944cd4dd30c460a1246cd5f3fd98
SiWickham <simon.wickham1@nhs.net>
2026-06-11 13:32:13 +0100

    Converstion of LA plot to function for reproduction in qmd commit to eth_dist.R

--- src/visuals/eth_dist.R
+++ src/visuals/eth_dist.R
@@ -1,16 +1,19 @@
 
 # Borough -----------------------------------------------------------------
 
+plot_LA_eth <- function(df, borough) {
+
 # Ethnic group by LAD distribution
 
-plot_eth <- unified_eth_LA_per |>
+plot_eth <- df |>
+  filter(`Upper tier local authorities` == borough) |>
   pivot_longer(cols = c(Pop_per, Ref_per),
                names_to = "Series",
                values_to = "Percent")
 
-LA_eth <- ggplot(plot_eth, aes(x = `Ethnic group (8 categories)`,
-                               y = Percent,
-                               fill = Series)) +
+ggplot(plot_eth, aes(x = `Ethnic group (8 categories)`,
+                     y = Percent,
+                     fill = Series)) +
   geom_col(position = position_dodge(width = 0.8),
            width = 0.7) +
   geom_errorbar(data = subset(plot_eth, Series == "Ref_per"),
@@ -18,8 +21,6 @@ LA_eth <- ggplot(plot_eth, aes(x = `Ethnic group (8 categories)`,
                     ymax = Percent + Confidence),
                 position = position_nudge(x = 0.2),
                 width = 0.2) +
-  facet_wrap(~ `Upper tier local authorities`,
-             ncol = 3) +
   scale_fill_manual(name = "Population Group",
                     values = c("Ref_per" = palette_tu[6],
                                "Pop_per" = palette_tu[7]),
@@ -27,10 +28,11 @@ LA_eth <- ggplot(plot_eth, aes(x = `Ethnic group (8 categories)`,
                                "New referrals")) +
   labs(x = "Ethnic group",
        y = "Percentage",
-       title = "Distribution of South London Boroughs referrals compared to the area population",
+       title = paste0("Distribution of ", borough," referrals compared to the area population"),
        subtitle = "Referrals received between May 2023 and April 2026",
        caption = "Source: Mental Health Services Data Set and Office of National Statistics") +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1),
+                     breaks = seq(0, 0.8, by = 0.1)) +
   scale_x_discrete(labels = function(Diagnosis) str_wrap(Diagnosis, width = 40)) +
   theme(text = element_text(family = "Franklin Gothic Book"),
         strip.background = element_rect(fill = palette_tu[1]),
@@ -47,23 +49,32 @@ LA_eth <- ggplot(plot_eth, aes(x = `Ethnic group (8 categories)`,
         legend.position = "bottom",
         legend.text = element_text(size = 7.5)) +
   coord_flip()
-
+}
 
 # Ethnic group by LA table
 
 LA_eth_tbl <- unified_eth_LA_per |>
   ungroup() |>
-  gt() |>
-  fmt_percent(columns = c(Pop_per, Ref_per),
-              decimals = 1) |>
-  cols_hide(columns = Confidence) |>
-  cols_label(`Upper tier local authorities` = "Borough",
-             `Ethnic group (8 categories)` = "Ethnic group",
-             Pop_per = "Population",
-             Ref_per = "Referrals") |>
-  tab_header(title = "Distribution of South London Boroughs referrals compared to the area population") |>
-  tab_style(style = list(cell_fill(color = palette_tu[1])),
-            locations = cells_column_labels(everything()))
+  select(`Upper tier local authorities`,
+         `Ethnic group (8 categories)`,
+         Pop_per,
+         Ref_per) |>
+  rename(Borough = `Upper tier local authorities`,
+         `Ethnic group` = `Ethnic group (8 categories)`,
+         Population = Pop_per,
+         Referrals = Ref_per) |>
+  datatable(caption = "Distribution of South London Boroughs referrals compared to the area population",
+            rownames = FALSE,
+            options = list(initComplete = JS(sprintf("function(settings, json) {
+                                                                                $(this.api().table().header()).find('th').css({
+                                                                                'background-color': '%s',
+                                                                                'color': 'white'
+                                                                                });
+                                                     }",
+                                                     palette_tu[1])))) |>
+  formatPercentage(columns = c("Population",
+                               "Referrals"),
+                   digits = 1)
 
 
 # South London ------------------------------------------------------------
@@ -116,14 +127,21 @@ Lon_eth <- ggplot(plot_LON_eth, aes(x = `Ethnic group (8 categories)`,
 
 Lon_eth_tbl <- unified_eth_LON_per |>
   ungroup() |>
-  gt() |>
-  fmt_percent(columns = c(Pop_per, Ref_per),
-              decimals = 1) |>
-  cols_hide(columns = c(Confidence,
-                        `Upper tier local authorities`)) |>
-  cols_label(`Ethnic group (8 categories)` = "Ethnic group",
-             Pop_per = "Population",
-             Ref_per = "Referrals") |>
-  tab_header(title = "Distribution of South London total referrals compared to the area population") |>
-  tab_style(style = list(cell_fill(color = palette_tu[1])),
-            locations = cells_column_labels(everything()))
+  select(`Ethnic group (8 categories)`,
+         Pop_per,
+         Ref_per) |>
+  rename(`Ethnic group` = `Ethnic group (8 categories)`,
+         Population = Pop_per,
+         Referrals = Ref_per) |>
+  datatable(caption = "Distribution of South London referrals compared to the area population",
+            rownames = FALSE,
+            options = list(initComplete = JS(sprintf("function(settings, json) {
+                                                                                $(this.api().table().header()).find('th').css({
+                                                                                'background-color': '%s',
+                                                                                'color': 'white'
+                                                                                });
+                                                     }",
+                                                     palette_tu[1])))) |>
+  formatPercentage(columns = c("Population",
+                               "Referrals"),
+                   digits = 1)
