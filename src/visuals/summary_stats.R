f54bb6dcc03a992362d1f28bb102fd0c3c389406
SiWickham <simon.wickham1@nhs.net>
2026-06-18 14:50:02 +0100

    Addition of autism and ld stats on referrals received commit to summary_stats.R

--- src/visuals/summary_stats.R
+++ src/visuals/summary_stats.R
@@ -26,3 +26,41 @@ fy_ref_new <- ref_new_fy |>
 
 fy_ref_pop <- cbind(fy_ref_new, pop_tot) |>
   mutate('population_rate' = (Referrals / LAD_Population) * 1000)
+
+
+# Other Flags -------------------------------------------------------------
+
+fy_ref_autld <- ref_new_fy |>
+  filter(Rejected_Flag == 0,
+         financial_year > 2022) |>
+  group_by(financial_year) |>
+  mutate('Aut_Only' = case_when(AutismFlag == '1' & LDFlag == '0' ~ 1,
+                                     TRUE ~ 0),
+         'LD_Only' = case_when(LDFlag == '1' & AutismFlag == '0' ~ 1,
+                               TRUE ~ 0),
+         'Aut/LD' = case_when(AutismFlag == '1' & LDFlag == '1' ~ 1,
+                                     TRUE ~ 0)) |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE),
+            'Aut' = sum(Aut_Only, na.rm = TRUE),
+            'LD' = sum(LD_Only, na.rm = TRUE),
+            'Aut&LD' = sum(`Aut/LD`, na.rm = TRUE)) |>
+  mutate('Aut%' = Aut / Referrals,
+         'LD%' = LD / Referrals,
+         'Aut&LD%' = `Aut&LD` / Referrals)
+
+all_ref_autld <- ref_new_fy |>
+  filter(Rejected_Flag == 0,
+         financial_year > 2022) |>
+  mutate('Aut_Only' = case_when(AutismFlag == '1' & LDFlag == '0' ~ 1,
+                                TRUE ~ 0),
+         'LD_Only' = case_when(LDFlag == '1' & AutismFlag == '0' ~ 1,
+                               TRUE ~ 0),
+         'Aut/LD' = case_when(AutismFlag == '1' & LDFlag == '1' ~ 1,
+                              TRUE ~ 0)) |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE),
+            'Aut' = sum(Aut_Only, na.rm = TRUE),
+            'LD' = sum(LD_Only, na.rm = TRUE),
+            'Aut&LD' = sum(`Aut/LD`, na.rm = TRUE)) |>
+  mutate('Aut%' = Aut / Referrals,
+         'LD%' = LD / Referrals,
+         'Aut&LD%' = `Aut&LD` / Referrals)
