7823f7c833248bd0d6997102293016708e4926ef
SiWickham <simon.wickham1@nhs.net>
2026-07-14 09:16:19 +0100

    Addition of SL and NON SL resident split commit to summary_stats.R

--- src/visuals/summary_stats.R
+++ src/visuals/summary_stats.R
@@ -1,5 +1,6 @@
 
-# Referrals split by FY
+# All Providers -----------------------------------------------------------
+
 
 ref_new_fy <- ref_new_proc |>
   mutate(financial_year = if_else(month(ReferralRequestReceivedDate) < 4,
@@ -25,4 +26,55 @@ fy_ref_new <- ref_new_fy |>
   mutate('Rejection%' = Rejections / Referrals)
 
 fy_ref_pop <- cbind(fy_ref_new, pop_tot) |>
+  mutate('population_rate' = (Referrals / LAD_Population) * 1000)
+
+
+
+# SL Resident ------------------------------------------------------------
+
+all_ref_new_SL <- ref_new_fy |>
+  filter(financial_year > 2022,
+         SL_Resident_Flag == "SL Resident") |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE),
+            'Rejections' = sum(Rejected_Flag, na.rm = TRUE)) |>
+  mutate('Rejection%' = Rejections / Referrals)
+
+all_ref_pop_SL <- cbind(all_ref_new_SL, pop_tot) |>
+  mutate('population_rate' = ((Referrals / LAD_Population) * 1000) / 3) # 3 years of data
+
+
+fy_ref_new_SL <- ref_new_fy |>
+  filter(financial_year > 2022,
+         SL_Resident_Flag == "SL Resident") |>
+  group_by(financial_year) |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE),
+            'Rejections' = sum(Rejected_Flag, na.rm = TRUE)) |>
+  mutate('Rejection%' = Rejections / Referrals)
+
+fy_ref_pop_SL <- cbind(fy_ref_new_SL, pop_tot) |>
+  mutate('population_rate' = (Referrals / LAD_Population) * 1000)
+
+
+# NON SL Resident ------------------------------------------------------------
+
+all_ref_new_NSL <- ref_new_fy |>
+  filter(financial_year > 2022,
+         SL_Resident_Flag == "Non-SL Resident") |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE),
+            'Rejections' = sum(Rejected_Flag, na.rm = TRUE)) |>
+  mutate('Rejection%' = Rejections / Referrals)
+
+all_ref_pop_NSL <- cbind(all_ref_new_NSL, pop_tot) |>
+  mutate('population_rate' = ((Referrals / LAD_Population) * 1000) / 3) # 3 years of data
+
+
+fy_ref_new_NSL <- ref_new_fy |>
+  filter(financial_year > 2022,
+         SL_Resident_Flag == "Non-SL Resident") |>
+  group_by(financial_year) |>
+  summarise('Referrals' = sum(New_referral, na.rm = TRUE),
+            'Rejections' = sum(Rejected_Flag, na.rm = TRUE)) |>
+  mutate('Rejection%' = Rejections / Referrals)
+
+fy_ref_pop_NSL <- cbind(fy_ref_new_NSL, pop_tot) |>
   mutate('population_rate' = (Referrals / LAD_Population) * 1000)
