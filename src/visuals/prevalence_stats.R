88bc31b98447d84b5074ac13ff9da5a575ab6f2c
SiWickham <simon.wickham1@nhs.net>
2026-07-17 15:48:53 +0100

    Creation of a prevalence table.

---
+++ src/visuals/prevalence_stats.R
@@ -0,0 +1,46 @@
+
+# Table of prevalence
+
+prev_pop <- data.frame(pop_tot_per$Age_band,
+                       pop_tot_per$Population,
+                       Estimate_Prevalence = c(0.043/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.081/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.013/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.013/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.013/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.013/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.013/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.013/0.448,       # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+                                               0.013/0.448)) |>   # research prevalence / proportion of existing diagnoses as Anorexia, atypical anorexia or bulimia
+  filter(pop_tot_per.Age_band != "Under 12") |>
+  mutate(Estimate_Population = pop_tot_per.Population * Estimate_Prevalence,
+         T3_referral = Estimate_Population * 0.2) |>
+  rename("Age_band" = pop_tot_per.Age_band,
+         "Population" = pop_tot_per.Population)
+
+prev_tbl <- left_join(prev_pop, ref_new_age_tot, by = c("Age_band" = "Age_band")) |>
+  mutate(Estimate_Population = round(Estimate_Population),
+         T3_referral = round(T3_referral),
+         Referrals = if_else(Referrals < 5,
+                             NA_real_,
+                             round(Referrals / 5) * 5)) |>
+  select(Age_band,
+         Population,
+         Estimate_Prevalence,
+         Estimate_Population,
+         T3_referral,
+         Referrals) |>
+  rename("Age band" = "Age_band",
+         "Estimate Prevalence" = "Estimate_Prevalence",
+         "Estimate Population" = "Estimate_Population",
+         "Requiring T3 Service Referrals" = "T3_referral",
+         "36 months of Referrals" = "Referrals") |>
+  gt() |>
+  fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals`, `36 months of Referrals`),
+             decimals = 0,
+             use_seps = TRUE) |>
+  fmt_percent(columns = c(`Estimate Prevalence`),
+              decimals = 1) |>
+  tab_header(title = "Estimated referral prevalence compared to received referrals") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
