921ecf1435fe52c5edb8a44bb3421c58b78906d1
SiWickham <simon.wickham1@nhs.net>
2026-08-20 11:22:03 +0100

    Addition of year break down for prevalence figures commit to prevalence_stats.R

--- src/visuals/prevalence_stats.R
+++ src/visuals/prevalence_stats.R
@@ -58,6 +58,92 @@ prev_tbl <- left_join(prev_pop, ref_new_age_tot, by = c("Age_band" = "Age_band")
             locations = cells_column_labels(everything()))
 
 
+prev_tbl_y1 <- left_join(prev_pop, ref_new_age_tot_yr1, by = c("Age_band" = "Age_band")) |>
+  mutate(Estimate_Population = round(Estimate_Population),
+         T3_referral = round(T3_referral),
+         Referrals = if_else(Referrals < 5,
+                             NA_real_,
+                             round(Referrals / 5) * 5)) |>
+  select(Age_band,
+         Population,
+         Estimate_Prevalence,
+         Estimate_Population,
+         T3_referral,
+         Referrals) |>
+  rename("Age band" = "Age_band",
+         "Estimate Prevalence" = "Estimate_Prevalence",
+         "Estimate Population" = "Estimate_Population",
+         "Requiring T3 Service Referrals (12 months)" = "T3_referral",
+         "Referrals (year 1)" = "Referrals") |>
+  gt() |>
+  fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals (12 months)`, `Referrals (year 1)`),
+             decimals = 0,
+             use_seps = TRUE) |>
+  fmt_percent(columns = c(`Estimate Prevalence`),
+              decimals = 1) |>
+  tab_header(title = "Estimated referral prevalence compared to received referrals") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
+
+
+prev_tbl_y2 <- left_join(prev_pop, ref_new_age_tot_yr2, by = c("Age_band" = "Age_band")) |>
+  mutate(Estimate_Population = round(Estimate_Population),
+         T3_referral = round(T3_referral),
+         Referrals = if_else(Referrals < 5,
+                             NA_real_,
+                             round(Referrals / 5) * 5)) |>
+  select(Age_band,
+         Population,
+         Estimate_Prevalence,
+         Estimate_Population,
+         T3_referral,
+         Referrals) |>
+  rename("Age band" = "Age_band",
+         "Estimate Prevalence" = "Estimate_Prevalence",
+         "Estimate Population" = "Estimate_Population",
+         "Requiring T3 Service Referrals (12 months)" = "T3_referral",
+         "Referrals (year 2)" = "Referrals") |>
+  gt() |>
+  fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals (12 months)`, `Referrals (year 2)`),
+             decimals = 0,
+             use_seps = TRUE) |>
+  fmt_percent(columns = c(`Estimate Prevalence`),
+              decimals = 1) |>
+  tab_header(title = "Estimated referral prevalence compared to received referrals") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
+
+
+prev_tbl_y3 <- left_join(prev_pop, ref_new_age_tot_yr3, by = c("Age_band" = "Age_band")) |>
+  mutate(Estimate_Population = round(Estimate_Population),
+         T3_referral = round(T3_referral),
+         Referrals = if_else(Referrals < 5,
+                             NA_real_,
+                             round(Referrals / 5) * 5)) |>
+  select(Age_band,
+         Population,
+         Estimate_Prevalence,
+         Estimate_Population,
+         T3_referral,
+         Referrals) |>
+  rename("Age band" = "Age_band",
+         "Estimate Prevalence" = "Estimate_Prevalence",
+         "Estimate Population" = "Estimate_Population",
+         "Requiring T3 Service Referrals (12 months)" = "T3_referral",
+         "Referrals (year 3)" = "Referrals") |>
+  gt() |>
+  fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals (12 months)`, `Referrals (year 3)`),
+             decimals = 0,
+             use_seps = TRUE) |>
+  fmt_percent(columns = c(`Estimate Prevalence`),
+              decimals = 1) |>
+  tab_header(title = "Estimated referral prevalence compared to received referrals") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
+
+
+# BEAT Prevalence
+
 prev_beat_tbl <- left_join(prev_pop, ref_new_age_tot, by = c("Age_band" = "Age_band")) |>
   mutate(BEAT_Population = round(BEAT_Population),
          BEAT_T3_referral = round(BEAT_T3_referral),
@@ -77,6 +163,90 @@ prev_beat_tbl <- left_join(prev_pop, ref_new_age_tot, by = c("Age_band" = "Age_b
          "36 months of Referrals" = "Referrals") |>
   gt() |>
   fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals (12 months)`, `36 months of Referrals`),
+             decimals = 0,
+             use_seps = TRUE) |>
+  fmt_percent(columns = c(`Estimate Prevalence`),
+              decimals = 1) |>
+  tab_header(title = "BEAT report estimated referral prevalence compared to received referrals") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
+
+
+prev_beat_tbl_y1 <- left_join(prev_pop, ref_new_age_tot_yr1, by = c("Age_band" = "Age_band")) |>
+  mutate(BEAT_Population = round(BEAT_Population),
+         BEAT_T3_referral = round(BEAT_T3_referral),
+         Referrals = if_else(Referrals < 5,
+                             NA_real_,
+                             round(Referrals / 5) * 5)) |>
+  select(Age_band,
+         Population,
+         BEAT_Prevalence,
+         BEAT_Population,
+         BEAT_T3_referral,
+         Referrals) |>
+  rename("Age band" = "Age_band",
+         "Estimate Prevalence" = "BEAT_Prevalence",
+         "Estimate Population" = "BEAT_Population",
+         "Requiring T3 Service Referrals (12 months)" = "BEAT_T3_referral",
+         "Referrals (year 1)" = "Referrals") |>
+  gt() |>
+  fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals (12 months)`, `Referrals (year 1)`),
+             decimals = 0,
+             use_seps = TRUE) |>
+  fmt_percent(columns = c(`Estimate Prevalence`),
+              decimals = 1) |>
+  tab_header(title = "BEAT report estimated referral prevalence compared to received referrals") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
+
+
+prev_beat_tbl_y2 <- left_join(prev_pop, ref_new_age_tot_yr2, by = c("Age_band" = "Age_band")) |>
+  mutate(BEAT_Population = round(BEAT_Population),
+         BEAT_T3_referral = round(BEAT_T3_referral),
+         Referrals = if_else(Referrals < 5,
+                             NA_real_,
+                             round(Referrals / 5) * 5)) |>
+  select(Age_band,
+         Population,
+         BEAT_Prevalence,
+         BEAT_Population,
+         BEAT_T3_referral,
+         Referrals) |>
+  rename("Age band" = "Age_band",
+         "Estimate Prevalence" = "BEAT_Prevalence",
+         "Estimate Population" = "BEAT_Population",
+         "Requiring T3 Service Referrals (12 months)" = "BEAT_T3_referral",
+         "Referrals (year 2)" = "Referrals") |>
+  gt() |>
+  fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals (12 months)`, `Referrals (year 2)`),
+             decimals = 0,
+             use_seps = TRUE) |>
+  fmt_percent(columns = c(`Estimate Prevalence`),
+              decimals = 1) |>
+  tab_header(title = "BEAT report estimated referral prevalence compared to received referrals") |>
+  tab_style(style = list(cell_fill(color = palette_tu[1])),
+            locations = cells_column_labels(everything()))
+
+
+prev_beat_tbl_y3 <- left_join(prev_pop, ref_new_age_tot_yr3, by = c("Age_band" = "Age_band")) |>
+  mutate(BEAT_Population = round(BEAT_Population),
+         BEAT_T3_referral = round(BEAT_T3_referral),
+         Referrals = if_else(Referrals < 5,
+                             NA_real_,
+                             round(Referrals / 5) * 5)) |>
+  select(Age_band,
+         Population,
+         BEAT_Prevalence,
+         BEAT_Population,
+         BEAT_T3_referral,
+         Referrals) |>
+  rename("Age band" = "Age_band",
+         "Estimate Prevalence" = "BEAT_Prevalence",
+         "Estimate Population" = "BEAT_Population",
+         "Requiring T3 Service Referrals (12 months)" = "BEAT_T3_referral",
+         "Referrals (year 3)" = "Referrals") |>
+  gt() |>
+  fmt_number(columns = c(`Population`, `Estimate Population`, `Requiring T3 Service Referrals (12 months)`, `Referrals (year 3)`),
              decimals = 0,
              use_seps = TRUE) |>
   fmt_percent(columns = c(`Estimate Prevalence`),
