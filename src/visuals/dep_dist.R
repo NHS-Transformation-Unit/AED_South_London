e2e511b0fc0a636494beed177e6bb9e8117767ec
SiWickham <simon.wickham1@nhs.net>
2026-09-07 15:50:51 +0100

    Addition of South East vs South West London split table commit to dep_dist.R

--- src/visuals/dep_dist.R
+++ src/visuals/dep_dist.R
@@ -86,6 +86,37 @@ LA_dep_tbl <- left_join(unified_dep_LA_per, ref_new_dep_LA, by = c("Upper tier l
                    digits = 1)
 
 
+# Deprivation band by SL table
+
+SL_dep_tbl <- left_join(unified_dep_SL_per, ref_new_dep_SL, by = c("Upper tier local authorities" = "SL Side",
+                                                                   "IMD Decile" = "IMD19dec")) |>
+  ungroup() |>
+  mutate(Ref_new = if_else(Referrals < 5,
+                           NA_real_,
+                           round(Referrals / 5) * 5)) |>
+  select(`Upper tier local authorities`,
+         `IMD Decile`,
+         Pop_per,
+         Ref_per,
+         Ref_new) |>
+  rename(Borough = `Upper tier local authorities`,
+         Population = Pop_per,
+         Referrals = Ref_per,
+         `Rounded Referrals`= Ref_new) |>
+  datatable(caption = "Distribution of South London referrals compared to the area population",
+            rownames = FALSE,
+            options = list(initComplete = JS(sprintf("function(settings, json) {
+                                                                                $(this.api().table().header()).find('th').css({
+                                                                                'background-color': '%s',
+                                                                                'color': 'white'
+                                                                                });
+                                                     }",
+                                                     palette_tu[1])))) |>
+  formatPercentage(columns = c("Population",
+                               "Referrals"),
+                   digits = 1)
+
+
 # South London -------------------------------------------------------------
 
 # Deprivation band for London distribution
