84cdeb2244c2df60885b41fcdb394db8b733ab06
SiWickham <simon.wickham1@nhs.net>
2026-09-07 15:56:20 +0100

    Correction to dataframe call commit to gen_dist.R

--- src/visuals/gen_dist.R
+++ src/visuals/gen_dist.R
@@ -90,7 +90,7 @@ LA_gen_tbl <- left_join(unified_gen_LA_per, ref_new_gen_LA, by = c("Upper tier l
 
 # Gender group by SL table
 
-SL_gen_tbl <- left_join(unified_gen_SLA_per, ref_new_gen_SL, by = c("Upper tier local authorities" = "SL Side",
+SL_gen_tbl <- left_join(unified_gen_SL_per, ref_new_gen_SL, by = c("Upper tier local authorities" = "SL Side",
                                                                    "Sex (2 categories)" = "Gender_group")) |>
   ungroup() |>
   mutate(Ref_new = if_else(Referrals < 5,
